42ee53e73db1bccf7a93865d63776c6f6271bc66
Dr. Georg Fischer <georg.fischer@t-online.de>
2018-02-20 00:06:45 +0100

    getSingle

--- data/gen_expr.pl
+++ data/gen_expr.pl
@@ -2,6 +2,7 @@
 
 # Generate path expressions for FASS curves
 # @(#) $Id$
+# 2018-02-19: compute single element (x1000000)
 # 2018-02-18, Georg Fischer
 # usage:
 #     perl gen_expr.pl n
@@ -15,9 +16,16 @@
 use strict;
 
 my $max_width = shift(@ARGV); # maximum number of digits for nodes of the generated curve
-my $letters = "ABCDEFGHIJKLMNOPQRSTUVWXY"; # for widht <= 25
+my $max_size  = 0x7ffffff; # very high
+if ($max_width < 0) {
+    $max_size = - $max_width;
+    $max_width = 256; # very high
+}
+# global variables
+my $letters = "ABCDEFGHIJKLMNOPQRSTUVWXY"; # for width <= 25
+my %pexprs  = (); # stores the generated subexpressions
 my %lengths = (); # stores the lengths of the subexpressions
-
+my $last_name; # name of last path expression generated
 print <<'GFis';
 A0 = []; len = 1;
 #----
@@ -25,34 +33,43 @@ GFis
 $lengths{"A0"} = 1; # strange, empty = 1 element?
 
 my $width = 1;
-while ($width <= $max_width) {
+my $size = 0; # of generated path expression
+while ($width <= $max_width and $size <= $max_size) {
     my $level = 0;
     while ($level <= $width) {
         my $letter = substr($letters, $level, 1);
-        &genOuter($width, $level);
+        $size = &genOuter($width, $level);
         $level ++;
     } # while $Level
     print "#--------\n";
     $width ++;
 } # while $width
-
+if ($max_width == 256) { # max_size was specified: get a single element
+    &getSingle($last_name, $max_size);
+} # single
+# end of main
+#-------------------------------
 sub genInner { # generate a path description for the "inner S" structure from [1,2,3]
     # A99= [A98.98+1,/A98.98+2,A98.98+3];
     my ($width, $level) = @_;
     my $width_1 = $width - 1;
     my $name_00 = substr($letters, $level, 1) . $width;
     my $name_01 = substr($letters, $level, 1) . $width_1;
+    my $pexpr;
     my $result;
     if (1) {
-        $result = "[$name_01.$width_1+1"
-                .",/$name_01.$width_1+2"
-                . ",$name_01.$width_1+3]";
+        $pexpr = "[$name_01.$width_1+1"
+               .",/$name_01.$width_1+2"
+               . ",$name_01.$width_1+3]";
+        $result = $lengths{$name_01} * 3;
+        $lengths{$name_00} = $result;
+        $pexprs{$name_00}  = $pexpr;
+        print "$name_00 = " . $pexpr . "; len = " . $result . " = $lengths{$name_01} * 3;\n";
+        $last_name = $name_00;
     }
-    $lengths{$name_00} = $lengths{$name_01} * 3;
-    print "$name_00 = " . $result . "; len = " . $lengths{$name_00} . ";\n";
     return $result;
 } # genInner
-
+#-------------------------------
 sub genOuter { # generate a path description for the "outer meander" structure from [0, inner, 4]
     my ($width, $level) = @_;
     my $width_1 = $width - 1;
@@ -67,29 +84,101 @@ sub genOuter { # generate a path description for the "outer meander" structure f
         my $temp = $x0; $x0 = $x4; $x4 = $temp;
     }
     my $result;
+    my $pexpr;
     if ($level == 0) {
         $result = &genInner($width, $level);
     } else {
         if ($level == 1) {
             if ($width % 2 == 0) { # even
-            $result = "[$name_11.$level_1+$x4"
-                    .",/$name_10"
-                    . ",$name_11.$level_1+$x0]";
+            $pexpr = "[$name_11.$level_1+$x4"
+                   .",/$name_10"
+                   . ",$name_11.$level_1+$x0]";
             } else {               # odd
-            $result ="[/$name_11.$level_1+$x4"
-                    . ",$name_10"
-                    .",/$name_11.$level_1+$x0]";
+            $pexpr ="[/$name_11.$level_1+$x4"
+                   . ",$name_10"
+                   .",/$name_11.$level_1+$x0]";
             }
         } else {
-            $result = "[$name_11.$level_1+$x4"
-                    . ",$name_10"
-                    . ",$name_11.$level_1+$x0]";
+            $pexpr = "[$name_11.$level_1+$x4"
+                   . ",$name_10"
+                   . ",$name_11.$level_1+$x0]";
         }
-        $lengths{$name_00} = $lengths{$name_11} + $lengths{$name_10} + $lengths{$name_11};
-        print "$name_00 = " . $result . "; len = " . $lengths{$name_00} . ";\n";
+        $result   =  $lengths{$name_11} + $lengths{$name_10} + $lengths{$name_11};
+        my $terms = "$lengths{$name_11} + $lengths{$name_10} + $lengths{$name_11}";
+        $lengths{$name_00} = $result;
+        $pexprs{$name_00}  = $pexpr;
+        print "$name_00 = " . $pexpr . "; len = " . $result . " = $terms;\n";
+        $last_name = $name_00;
     }
     return $result;
 } # genOuter
+#-------------------------------
+sub getSingle {
+    my ($last_name, $last_index) = @_;
+    my $pname = $last_name;  # of current pexpr
+    my $index = $last_index; # always in current pexpr
+    if ($index >= $lengths{$pname}) {
+        print STDERR "element [$last_index] is not contained in $last_name\n";
+        return 0;
+    }
+    my $loop_check = 128;
+    my @inserts = ();
+    while ($pname ne "A0" and $loop_check >= 0) {
+        $loop_check --;
+        my $pexpr = $pexprs{$pname};
+        print "# $pname = $pexpr; len = $lengths{$pname}; index = $index;\n";
+        $pexpr =~ s{[\;\[\] ]}{}g; # remove all superfluous chars.
+        my @parts = split(/\,/, $pexpr);
+        my $ipart = 0;
+        my $busy = 1;
+        # find out in which of the parts the element [$index] is contained
+        my $sum = 0;
+        my $old_sum = 0;
+        while ($busy == 1 and $ipart < scalar(@parts)) {
+            my $pelem = $parts[$ipart];
+            my ($name, $exp, $digit) = ("", "", "");
+            ($name, $exp, $digit) = split(/[\+\.]/, $pelem);
+            my $reversed = 0;
+            if ($name =~ m{\A\/}) {
+                $reversed = 1;
+                $name =~ s{\A\/}{};
+            }
+            my $size = $lengths{$name};
+            $old_sum = $sum;
+            $sum += $size;
+            if ($index < $sum) { # it is contained in this part
+                $index -= $old_sum;
+                if ($reversed != 0) {
+                    $index = $size - $index; 
+                }
+                $busy = 0; # now examine $name
+                my $insert = "";
+                if ($exp ne "") {
+                    $insert = "$exp+$digit";
+                    push(@inserts, $insert);
+                }
+                print "# " . ($reversed != 0 ? "/" : "") . "$ipart -> " 
+                        . $name . "[$index].$insert\n";
+                $pname = $name;
+            } # if contained
+            $ipart ++;
+        } # while parts
+    } # while ne "A0"
+    print "# inserts = (" . join(",", @inserts) . ");\n";
+    # inserts = (2+0,1+0,0+0,1+3,0+2);
+    # now pop all inserts and build the final element
+    my $elem = "";
+    while (scalar(@inserts) > 0) {
+        my $insert = pop(@inserts);
+        my ($exp, $digit) = split(/\+/, $insert);
+        my $len = length($elem);
+        $elem = substr($elem, 0, $len - $exp) . $digit 
+              . substr($elem, $len - $exp);
+        print "# elem = $elem\n";
+    } # while @inserts
+    print $last_name . "[$last_index] = $elem(5);\n";
+} # getSingle
+#-------------------------------
 __DATA__
 georg@nunki:~/work/gits/fasces/data$ perl gen_expr.pl 5 
 A0 = []; len = 1;
