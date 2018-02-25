01ff4513207aa1fa0eeca32cd06737e58e319c6b
Dr. Georg Fischer <georg.fischer@t-online.de>
2018-02-25 21:29:05 +0100

    lists in A257705 and A257883

--- oeis/negpos/negpos.pl
+++ oeis/negpos/negpos.pl
@@ -3,6 +3,7 @@
 # Generate OEIS Sequence A131393 and its companions
 # 2018-02-24, Georg Fischer
 #------------------------------------------------------
+# C.f. list of sequences in https://oeis.org/search?q=A257705
 # usage:
 #   perl negpos.pl [noeis [n [oper a1 d1]]]
 #       noeis = "131388|131389|1313393|131394..."
@@ -50,15 +51,21 @@ my $dk = $d1; my $dkm1 = $dk; my %dset = ($dk, $k);
 print "# http://oeis.org/A$noeis/b$noeis.txt:"
         . " table n,a(n),n=1..$n\n";
 #    print "# ak = $ak, dk = $dk, akm1 = $akm1, dkm1 = $dkm1 \n";
-print "$k " . (($oper eq "ak") ? $ak : $dk) . "\n";
+    if (0) {
+    } elsif ($oper eq "ak") {
+        print "$k $ak\n";
+    } elsif ($oper eq "dk") {
+        print "$k $dk\n";
+    }
+my $busy;
 $k ++;
 while ($k <= $n) {
-    my $busy = 1;
+    $busy = 1;
     $dk = -1;
-    if (($noeis =~ m{131393|131394}) && $dkm1 < 0) { 
+    if (($noeis =~ m{131393|131394|131395|131396|131397}) && $dkm1 < 0) { 
         $dk = $dkm1 - 1;
-    } # else 131388|131389: without this condition
-    while ($busy == 1 and $dk > 1 - $akm1) { # negative
+    } # else 131388|131389 etc. without this condition
+    while ($busy == 1 and $dk > $d1 - $akm1) { # negative
         $ak = $akm1 + $dk;
         if (!defined($aset{$ak}) and !defined($dset{$dk} and $ak>0)) {
             $busy=0; $aset{$ak} = $k;         $dset{$dk}=$k;
@@ -79,21 +86,51 @@ while ($k <= $n) {
     } # while positive
     if (0) {
     } elsif ($oper eq "ak") {
-    	print "$k $ak";
+        print "$k $ak\n";
     } elsif ($oper eq "dk") {
-    	print "$k $dk";
+        print "$k $dk\n";
     }
-    # print "\t# ak = $ak, dk = $dk, akm1 = $akm1, dkm1 = $dkm1";
-    print "\n";
+    # print "\t# ak = $ak, dk = $dk, akm1 = $akm1, dkm1 = $dkm1\n";
     $akm1 = $ak; $dkm1 = $dk;
     $k ++; # iterate
 } # while $k
-if (1) { # output of operations other than "ak", "dk"
+#--------
+if ($oper !~ m{ak|dk}) { # output of operations other than "ak", "dk"
+    my @ainv = sort(map { $_ = sprintf("%06d %d", $_, $aset{$_}); $_ } keys(%aset));
+    my @dpos = sort(map { $_ = sprintf("%06d %d", $dset{$_}, $_); $_ } keys(%dset));
+    my $temp = shift(@dpos); # accounts for positions "-1"
+    # print join("\n", @dpos) . "\n";
     if (0) {
-    } elsif ($oper eq "dp") {
-    	print "$k $ak";
-    } elsif ($oper eq "dn") {
-    	print "$k $dk";
+    } elsif ($oper =~ m{in}) {
+        $k = 0;
+        $busy = 1;
+        while ($busy == 1 and $k < scalar(@ainv)) {
+            my ($j, $aj) = split(/ /, $ainv[$k]);
+            $j += 0; # removes the leading zeroes
+            $busy = ($j == $k + 1 ? 1 : 0);
+            if ($busy == 1) {
+                print "$j $aj\n";
+            }
+            $k ++;
+        } # while $k
+    } elsif ($oper =~ m{dp}) {
+        my $k = 0;
+        print join("", map { my ($j, $dj) = split(/\s+/); 
+                $j --; $_ = "";
+                if ($dj > 0) {
+                    $k ++;
+                    $_ = "$k $j\n";
+                }
+                $_ } @dpos) . "\n";
+    } elsif ($oper =~ m{dn}) {
+        my $k = 0;
+        print join("", map { my ($j, $dj) = split(/\s+/); 
+                $j --; $_ = "";
+                if ($dj < 0) {
+                    $k ++;
+                    $_ = "$k $j\n";
+                }
+                $_ } @dpos) . "\n";
     }
 } # other oper
 # https://oeis.org/wiki/User:Georg_Fischer Feb. 24, 2018
