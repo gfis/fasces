bfa1c6844815e68dbf61b7d00c9162b56fbde508
Georg Fischer <dr.Georg.Fischer@gmail.com>
2018-09-05 10:15:44 +0200

    -a west|east|free

--- oeis/collatz/collatz_rails.pl
+++ oeis/collatz/collatz_rails.pl
@@ -3,6 +3,7 @@
 # https://github.com/gfis/fasces/blob/master/oeis/collatz/collatz_rails.pl
 # Print a directory of railways in the Collatz graph
 # @(#) $Id$
+# 2018-09-05: new kernel format; -a west|east|free
 # 2018-08-30, Georg Fischer: derived from collatz_roads.pl
 #
 # The algorithm is the same as in collatz_roads.pl,
@@ -29,7 +30,7 @@
 # 142/104: [142 m  71 d 214 m 107 d 322 m 161 d 484 m  242 m 121 d | 364 m 182, 91, ... 10, 5, 16, 8, 4, 2, 1]
 #            +1  *6+4    +1  *6+4    +1  *6+4    +1   *6+4  *6+2     =     =   ...
 # 143/104: [143 d 430 m 215 d 646 m 323 d 970 m 485 d 1456 m 728 m | 364 m 182, 91, ... 10, 5, 16, 8, 4, 2, 1]
-# 
+#
 # continuation:
 # 124 m 62 m  31 d 94 m  47 d 142 m
 #  +2   +1  *6+4    +1 *6+4    +1
@@ -48,6 +49,11 @@ my $incr6  = 6;
 my $start  = $start4;
 my $incr   = $incr6;
 my $action = "simple";
+my %text   =
+    ( "simple",     "Directory"
+    , "contig",     "Tree"
+    , "west",       "Plan West"
+    );
 my $mode   = "html";
 while (scalar(@ARGV) > 0 and ($ARGV[0] =~ m{\A\-})) {
     my $opt = shift(@ARGV);
@@ -78,7 +84,6 @@ while ($ffrail < $start4) { # $rails[0..3] are not used
     $ffrail ++;
 } # while not used
 my @nums   = @rails; # defined if the number was visited
-$ffrail    = scalar(@rails); # (is asserted)
 my @queue;
 my $index = 0;
 #----------------
@@ -86,13 +91,16 @@ my $index = 0;
 if (0) { # switch action
 
 } elsif ($action =~ m{simple}) { # straightforward incrementing of the start values
+    $ffrail = scalar(@rails); # (is asserted)
     while ($ffrail < $maxn) {
         @rails[$ffrail] = &build_rail($ffrail);
         $ffrail += $incr6;
     } # while $ffrail
+    &print_rails();
 
 } elsif ($action =~ m{contig}) { # identify contiguous blocks of start values
-    @queue = ($ffrail);
+    $ffrail = scalar(@rails); # (is asserted)
+    @queue  = ($ffrail);
     while (scalar(@queue) > 0) {
         my $selem = &dequeue();
         $index ++;
@@ -109,23 +117,93 @@ if (0) { # switch action
             }
         }
     } # while $selem
+    &print_rails();
+
+} elsif ($action =~ m{west|east|free}) { # follow railways down to the capital
+    $ffrail = scalar(@rails); # (is asserted)
+    while ($ffrail < $maxn) {
+        @rails[$ffrail] = &build_west($ffrail);
+        $ffrail += $incr6;
+    } # while $ffrail
+    &print_west();
+
+} elsif ($action =~ m{test}) { # test the long hypothesis
+    while (<DATA>) {
+        next if m{^\s*\#};
+        s{\s+\Z}{}; # chompr
+        my $line = $_;
+        my @row = split(/\s+/, $_);
+        my $west = &go_west($row[1]);
+        my $wkern = &to_kernel($west);
+        print "$line\t$west\t$wkern\t"
+        . ($wkern eq $row[5] ? "ok" : "not ok")
+        . "\n";
+    } # while DATA
 
 } else {
     die "invalid action \"$action\"\n";
 } # switch action
 #----------------
-# output the resulting array
-&print_table_head();
-my $irail = $start;
-while ($irail < $maxn) {
-    if (! defined($rails[$irail])) {
-        $irail = $maxn; # break loop
-    } else {
-        &print_rail($irail);
-    }
-    $irail += $incr;
-} # while $irail
-&print_table_tail();
+sub print_rails {
+    # output the resulting array
+    &print_rails_head();
+    my $irail = $start;
+    while ($irail < $maxn) {
+        if (! defined($rails[$irail])) {
+            $irail = $maxn; # break loop
+        } else {
+            &print_rail($irail);
+        }
+        $irail += $incr;
+    } # while $irail
+    &print_rails_tail();
+} # print_rails
+#----------------
+sub print_west {
+    print <<"GFis";
+<table>
+GFis
+    my $irail = $start;
+    while ($irail < $maxn) {
+        my @rail  = split(/$sep/, $rails[$irail]);
+        print "<tr>"
+            . "<td class=\"\">&nbsp;" . (scalar(@rail) / 2) . "&nbsp;</td>"
+            . "<td class=\"\">&nbsp;" . $irail              . "&nbsp;</td>";
+        my $ir;
+        if (0) {
+        } elsif ($action =~ m{west}) {
+            $ir = 0;
+            while ($ir < scalar(@rail)) {
+            #   print "<td class=\"d4\">&nbsp;$rail[$ir    ]&nbsp;</td>";
+                my $class = ($rail[$ir + 1] =~ m{\A0\.0\.}) ? "d5" : "";
+                print "<td class=\"$class\"  >&nbsp;$rail[$ir + 1]&nbsp;</td>"; # kernel
+                $ir += 2;
+            } # while $ir
+        } elsif ($action =~ m{east}) {
+            $ir = scalar(@rail) - 2;
+            while ($ir >= 0) {
+            #   print "<td class=\"d4\">&nbsp;$rail[$ir    ]&nbsp;</td>";
+                my $class = ($rail[$ir + 1] =~ m{\.\Z}) ? "arl d5" : "arl";
+                print "<td class=\"$class\"  >&nbsp;$rail[$ir + 1]&nbsp;</td>"; # kernel
+                $ir -= 2;
+            } # while $ir
+        } elsif ($action =~ m{free}) {
+            $ir = scalar(@rail) - 2;
+            while ($ir >= 0) {
+            #   print "<td class=\"d4\">&nbsp;$rail[$ir    ]&nbsp;</td>";
+                if ($rail[$ir + 1] =~ m{\.\Z}) {
+                	print "<td class=\"arl d5\"  >&nbsp;$rail[$ir + 1]&nbsp;</td>"; # kernel
+                }
+                $ir -= 2;
+            } # while $ir
+        } # switch action
+        print "</tr>\n";
+        $irail += $incr;
+    } # while $irail
+    print <<"GFis";
+</table>
+GFis
+} # print_west
 #----------------
 # termination
 if (0) {
@@ -199,31 +277,87 @@ sub build_rail { # build and return a single rail starting with $selem
     return join($sep, @result);
 } # build_rail
 #----------------
+sub build_west { # build and return a single rail starting with $selem
+    my ($elem)  = @_;
+    if ($debug >= 1) {
+        print "<!--build_west1: " . join("\t", $elem) . "-->\n";
+    }
+    my @result  = ();
+    my $busy    = 1; # as long as we can still do another step
+    while ($busy == 1) { # while 4 not reached
+        my $lkern  = &to_kernel($elem);
+        if ($debug >= 1) {
+            print "<!--build_west2: " . join("\t", $elem, $lkern) . "-->\n";
+        }
+        push(@result, $elem, $lkern);
+        if ($lkern ne "1.") {
+            $elem = &go_west($elem);
+        } else {
+            $busy = 0;
+        }
+    } # while 4 not reached
+    return join($sep, @result);
+} # build_west
+#----------------
 sub dequeue { # return lowest queue element
     @queue = sort {$a <=> $b} @queue;
     my $elem = shift(@queue);
     return $elem;
 } # dequeue
 #----------------
-sub get_kernel { # for n6-2, return the 2-3-free factor of n
+sub to_kernel { # for n6-2, return the 2-3-free factor and the exponents for 2 and 3 of n
     my ($parm) = @_;
     my $result = 0;
     if ($parm % $incr6 == $start4) {
         my $num = $parm / $incr6 + 1; # we had 6*(n-1) + 4
-        my $log3 = 0;
+        my $exp3 = 0;
         while ($num % 3 == 0 and $num > 0) {
-            $log3 ++;
+            $exp3 ++;
             $num /= 3;
         } # while 3
-        my $log2 = 0;
+        my $exp2 = 0;
         while ($num % 2 == 0 and $num > 0) {
-            $log2 ++;
+            $exp2 ++;
             $num /= 2;
         } # while 2
-        $result = "$log3.$log2.$num";
+        $result = $num;
+        if ($exp2 != 0) {
+            $result .= ".$exp2";
+        }
+        if ($exp3 != 0) {
+            $result .= ":$exp3";
+        }
+        if ($exp2 + $exp3 == 0) {
+            $result .= ".";
+        }
     }
     return $result;
-} # get_kernel
+} # to_kernel
+#----------------
+sub from_kernel { # for a kernel, return n
+    my ($elem)  = @_;
+    my @kern    = split(/[\.\:]/, $elem);
+    $kern[1] = 0 if ($kern[1] eq "");
+    $kern[2] = 0 if (scalar(@kern) == 2);
+    my $result = $kern[0] * 2**$kern[1] * 3**$kern[2];
+    return $result;
+} # from_kernel
+#----------------
+sub go_west {
+    # 82=7.1   -> 376=7.0:2, but for
+    # 46=1.3   -> 160=1.0:3
+    my ($elem)  = @_;
+    my @kern    = split(/[\.\:]/, &to_kernel($elem));
+    $kern[1] = 0 if $kern[1] eq "";
+    # $kern[2] = 0 if scalar(@kern) == 2;
+    $kern[2] = $kern[1];
+    $kern[1] = 0;
+    if (($kern[0] / 2) % 2 == $kern[2] % 2) {
+        $kern[2] ++;
+    }
+    my $result = 6 * ($kern[0] * 3**$kern[2]) - 2;
+    return $result / 4;
+} # go_west
 #----------------
 sub enqueue { # queue the parameter
     my ($elem) = @_;
@@ -256,14 +390,14 @@ sub print_rail {
                 . "<td class=\"arc    \">$rail[0]</td>"
                 . &cell_html(            $rail[1], "bor", $ir, "");
             if (0) {
-            print &cell_html($rail[$ir], " btr", $ir, ""); 
-        	}
+            print &cell_html($rail[$ir], " btr", $ir, "");
+            }
             $ir += 2;
             while ($ir < scalar(@rail)) {
                 my $id = "";
                 if (      $rail[$ir    ] % $incr6 == $start4) {
                     $id = $rail[$ir    ];
-                } 
+                }
                 if ($ir > 5 and $rail[$ir - 1] % $incr6 == $start4) {
                     $id = $rail[$ir - 1];
                     # print STDERR "$id\n";
@@ -273,11 +407,11 @@ sub print_rail {
             } # while $ir
             print "</tr>\n";
 
-			my $len = 0;
-			$ir = 5;
+            my $len = 0;
+            $ir = 5;
             while ($ir < scalar(@rail)) { # length is number of highlighted elements >= [4]
                 if (      $rail[$ir    ] % $incr6 == $start4) {
-                	$len ++;
+                    $len ++;
                 }
                 $ir ++;
             } # while len
@@ -285,9 +419,9 @@ sub print_rail {
             print "<tr>"
                 . "<td class=\"arl\">\&nbsp;</td>"
                 . "<td class=\"arr ker\">"
-                . &get_kernel($rail[1]) # $rail[1] #
+                . &to_kernel($rail[1]) # $rail[1] #
                 . "\&gt;"
-                . &get_kernel($rail[5])
+                . &to_kernel($rail[5])
                 . ",$len</td>";
             $ir = 2;
             while ($ir < scalar(@rail)) {
@@ -306,9 +440,9 @@ sub cell_html { # print one table cell
     my $rest = $elem % $incr6;
     my $result = "<td";
     if ($rest == $start4) {
-        $result .= " title=\"$ir:" . &get_kernel($elem) . "\"";
+        $result .= " title=\"$ir:" . &to_kernel($elem) . "\"";
     }
-    if ($id ne "") { 
+    if ($id ne "") {
         $result .= " id=\"$id\"";
         # print STDERR "id2: $id\n";
     }
@@ -335,7 +469,7 @@ sub print_html_head {
 ]>
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<title>3x+1 railway directory</title>
+<title>3x+1 Railway $text{$action}</title>
 <meta name="generator" content="https://github.com/gfis/fasces/blob/master/oeis/collatz/collatz_rails.pl" />
 <meta name="author"    content="Georg Fischer" />
 <style>
@@ -346,12 +480,12 @@ td      { padding-right: 4px; }
 tr,td,th{ text-align: right; }
 /* from https://stackoverflow.com/questions/10732690/offsetting-an-html-anchor-to-adjust-for-fixed-header
 does not work
-.anchor   { 
+.anchor   {
           display: block; position: relative;
           top: -250px; visibility: hidden; }
 .anch { margin-top: -300px;        /* Size of fixed header */
-        padding-bottom: 300px; 
-        display: block;} 
+        padding-bottom: 300px;
+        display: block;}
 */
 .arr    { background-color: white          ; color: black; }
 .arc    { background-color: white          ; color: black; text-align: center;      }
@@ -361,7 +495,7 @@ does not work
           border-right : 1px solid gray    ; border-bottom: 1px solid gray ; }
 .btr    { border-left  : 1px solid gray    ; border-top   : 1px solid gray ;
           border-right : 1px solid gray    ; }
-.bbr    { border-left  : 1px solid gray    ; 
+.bbr    { border-left  : 1px solid gray    ;
           border-right : 1px solid gray    ; border-bottom: 1px solid gray ; }
 .d0     { background-color: lemonchiffon   ; color: black;                   }
 .d1     { background-color: lavender       ; color: black;                   }
@@ -375,11 +509,11 @@ does not work
 </style>
 </head>
 <body>
-<h3>3x+1 railway directory</h3>
+<h3>3x+1 Railway $text{$action}</h3>
 GFis
 } # print_html_head
 #----------------
-sub print_table_head {
+sub print_rails_head {
     return if $mode ne "html";
     print <<"GFis";
 <p>
@@ -411,41 +545,41 @@ tree root &lt;-&nbsp;&nbsp;&nbsp;numbers &#x2261;
 <tr>
 <td class="arc    ">n</td>
 <td class="arr bor"><strong>6n&#8209;2</strong></td>
-<td class="arc btr">m</td>
-<td class="arc btr">m</td>
-<td class="arc btr">d</td>
-<td class="arc btr">m</td>
-<td class="arc btr">d</td>
-<td class="arc btr">m</td>
-<td class="arc btr">d</td>
-<td class="arc btr">m</td>
-<td class="arc btr">d</td>
+<td class="arc btr">&micro;</td>
+<td class="arc btr">&micro;</td>
+<td class="arc btr">&delta;</td>
+<td class="arc btr">&micro;</td>
+<td class="arc btr">&delta;</td>
+<td class="arc btr">&micro;</td>
+<td class="arc btr">&delta;</td>
+<td class="arc btr">&micro;</td>
+<td class="arc btr">&delta;</td>
 <td class="arc    ">...</td>
 </tr>
 <tr>
 <td class="arc    "></td>
 <td class="arr ker"></td>
-<td class="arc bbr">d</td>
-<td class="arc bbr">m</td>
-<td class="arc bbr">m</td>
-<td class="arc bbr">d</td>
-<td class="arc bbr">m</td>
-<td class="arc bbr">d</td>
-<td class="arc bbr">m</td>
-<td class="arc bbr">d</td>
-<td class="arc bbr">m</td>
+<td class="arc bbr">&delta;</td>
+<td class="arc bbr">&micro;</td>
+<td class="arc bbr">&micro;</td>
+<td class="arc bbr">&delta;</td>
+<td class="arc bbr">&micro;</td>
+<td class="arc bbr">&delta;</td>
+<td class="arc bbr">&micro;</td>
+<td class="arc bbr">&delta;</td>
+<td class="arc bbr">&micro;</td>
 <td class="arc    ">...</td>
 </tr>
 GFis
-} # print_table_head
+} # print_rails_head
 #----------------
-sub print_table_tail {
+sub print_rails_tail {
     return if $mode ne "html";
     print <<"GFis";
 </table>
 <p>End of directory</p>
 GFis
-} # print_table_tail
+} # print_rails_tail
 #----------------
 sub print_html_tail {
     return if $mode ne "html";
@@ -457,172 +591,59 @@ GFis
 } # print_html_tail
 #================================
 __DATA__
+<!--build_west1: 82-->
+<!--build_west2: 82     7.1-->
+<!--build_west2: 94     1.4-->
+<!--build_west2: 364    61.-->
+<!--build_west2: 274    23.1-->
+<!--build_west2: 310    13.2-->
+<!--build_west2: 526    11.3-->
+<!--build_west2: 1336   223.-->
+<!--build_west2: 334    7.3-->
+<!--build_west2: 850    71.1-->
+<!--build_west2: 958    5.5-->
+<!--build_west2: 1822   19.4-->
+<!--build_west2: 2308   385.-->
+<!--build_west2: 1732   289.-->
+<!--build_west2: 1300   217.-->
+<!--build_west2: 976    163.-->
+<!--build_west2: 244    41.-->
+<!--build_west2: 184    31.-->
+<!--build_west2: 46     1.3-->
+<!--build_west2: 40     7.-->
+<!--build_west2: 10     1.1-->
+<!--build_west2: 4      1.-->
 
-grep -E "\.5," kernels.tmp
-0.2.1>1.0.5,1
-0.1.17>3.0.5,5
-0.4.19>5.0.5,9
-0.1.1367>7.0.5,13
-0.2.6151>9.0.5,17
+<!--build_west1: 88-->
+<!--build_west2: 88     5:1-->
+<!--build_west2: 22     1.2-->
+<!--build_west2: 40     7.-->
+<!--build_west2: 10     1.1-->
+<!--build_west2: 4      1.-->
 
-grep -E "\.7," kernels.tmp
-0.1.1>0.0.7,0
-0.4.1>2.0.7,3
-0.1.71>4.0.7,7
-0.2.319>6.0.7,11
-0.1.5741>8.0.7,15
-0.3.12917>10.0.7,19
-
-grep -E "\.11," kernels.tmp
-1.0.1>0.0.11,0
-0.0.25>2.0.11,4
-0.0.223>4.0.11,8
-0.0.2005>6.0.11,12
-0.0.18043>8.0.11,16
-0.0.162385>10.0.11,20
-
-grep -E "\.13," kernels.tmp
-0.1.5>1.0.13,2
-0.3.11>3.0.13,6
-0.1.395>5.0.13,10
-0.2.1777>7.0.13,14
-0.1.31985>9.0.13,18
-
-grep -E "\.17," kernels.tmp
-0.0.13>1.0.17,1
-0.0.115>3.0.17,5
-0.0.1033>5.0.17,9
-0.0.9295>7.0.17,13
-0.0.83653>9.0.17,17
-
-grep -E "\.19," kernels.tmp
-0.0.5>0.0.19,0
-0.0.43>2.0.19,3
-0.0.385>4.0.19,7
-0.0.3463>6.0.19,11
-0.0.31165>8.0.19,15
-
-grep -E "\.23," kernels.tmp
-1.1.1>0.0.23,0
-0.2.13>2.0.23,4
-0.1.233>4.0.23,8
-0.5.131>6.0.23,12
-0.1.18863>8.0.23,16
-
-grep -E "\.25," kernels.tmp
-0.0.19>1.0.25,2
-0.0.169>3.0.25,6
-0.0.1519>5.0.25,10
-0.0.13669>7.0.25,14
-0.0.123019>9.0.25,18
-
-grep -E "\.29," kernels.tmp
-0.1.11>1.0.29,1
-0.2.49>3.0.29,5
-0.1.881>5.0.29,9
-0.4.991>7.0.29,13
-0.1.71351>9.0.29,17
-
-grep -E "\.31," kernels.tmp
-0.3.1>0.0.31,0
-0.1.35>2.0.31,3
-0.2.157>4.0.31,7
-0.1.2825>6.0.31,11
-0.5.1589>8.0.31,15
-
-grep -E "\.35," kernels.tmp
-2.0.1>0.0.35,0
-0.0.79>2.0.35,4
-0.0.709>4.0.35,8
-0.0.6379>6.0.35,12
-0.0.57409>8.0.35,16
-
-grep -E "\.37," kernels.tmp
-0.2.7>1.0.37,2
-0.1.125>3.0.37,6
-0.3.281>5.0.37,10
-0.1.10115>7.0.37,14
-
-grep -E "\.41," kernels.tmp
-0.0.31>1.0.41,1
-0.0.277>3.0.41,5
-0.0.2491>5.0.41,9
-0.0.22417>7.0.41,13
-
-grep -E "\.47," kernels.tmp
-1.2.1>0.0.47,0
-0.1.53>2.0.47,4
-0.3.119>4.0.47,8
-0.1.4283>6.0.47,12
-0.2.19273>8.0.47,16
-
-grep -E "\.49," kernels.tmp
-0.0.37>1.0.49,2
-0.0.331>3.0.49,6
-0.0.2977>5.0.49,10
-0.0.26791>7.0.49,14
-
-grep -E "\.53," kernels.tmp
-0.3.5>1.0.53,1
-0.1.179>3.0.53,5
-0.2.805>5.0.53,9
-0.1.14489>7.0.53,13
-
-grep -E "\.55," kernels.tmp
-0.1.7>0.0.55,0
-0.2.31>2.0.55,3
-0.1.557>4.0.55,7
-0.3.1253>6.0.55,11
-0.1.45107>8.0.55,15
-
-grep -E "\.59," kernels.tmp
-1.0.5>0.0.59,0
-0.0.133>2.0.59,4
-0.0.1195>4.0.59,8
-0.0.10753>6.0.59,12
-0.0.96775>8.0.59,16
-
-grep -E "\.61," kernels.tmp
-0.1.23>1.0.61,2
-0.2.103>3.0.61,6
-0.1.1853>5.0.61,10
-0.3.4169>7.0.61,14
-
-grep -E "\.65," kernels.tmp
-0.0.49>1.0.65,1
-0.0.439>3.0.65,5
-0.0.3949>5.0.65,9
-0.0.35539>7.0.65,13
-
-grep -E "\.67," kernels.tmp
-0.0.17>0.0.67,0
-0.0.151>2.0.67,3
-0.0.1357>4.0.67,7
-0.0.12211>6.0.67,11
-0.0.109897>8.0.67,15
-
-grep -E "\.71," kernels.tmp
-2.1.1>0.0.71,0
-0.5.5>2.0.71,4
-0.1.719>4.0.71,8
-0.2.3235>6.0.71,12
-0.1.58229>8.0.71,16
-
-grep -E "\.73," kernels.tmp
-0.0.55>1.0.73,2
-0.0.493>3.0.73,6
-0.0.4435>5.0.73,10
-0.0.39913>7.0.73,14
-
-grep -E "\.77," kernels.tmp
-0.1.29>1.0.77,1
-0.3.65>3.0.77,5
-0.1.2339>5.0.77,9
-0.2.10525>7.0.77,13
-
-grep -E "\.79," kernels.tmp
-0.2.5>0.0.79,0
-0.1.89>2.0.79,3
-0.6.25>4.0.79,7
-0.1.7199>6.0.79,11
-0.2.32395>8.0.79,15
+# Railways to capital (-a long)
+#   col1                col3n
+#i  a[i]    kern(a[i])  4a[i+1] kern(4a[i+1])  len
+1   82      0.1.7   ->  376     2.0.7           2   /4 ->
+2   94      0.4.1   ->  1456    5.0.1           5   /4 ->
+3   364     0.0.61  ->  1096    1.0.61          1   /4 ->
+4   274     0.1.23  ->  1240    2.0.23          2   /4 ->
+5   310     0.2.13  ->  2104    3.0.13          3   /4 ->
+6   526     0.3.11  ->  5344    4.0.11          4   /4 ->
+7   1336    0.0.223 ->  1336    0.0.223         0   /4 ->   =
+8   334     0.3.7   ->  3400    4.0.7           4   /4 ->
+9   850     0.1.71  ->  3832    2.0.71          2   /4 ->
+10  958     0.5.5   ->  7288    5.0.5           5   /4 ->   *
+11  1822    0.4.19  ->  9232    4.0.19          4   /4 ->   *
+12  2308    0.0.385 ->  6928    1.0.385         1   /4 ->
+13  1732    0.0.289 ->  5200    1.0.289         1   /4 ->
+14  1300    0.0.217 ->  3904    1.0.217         1   /4 ->
+15  976     0.0.163 ->  976     0.0.163         0   /4 ->   =
+16  244     0.0.41  ->  736     1.0.41          1   /4 ->
+17  184     0.0.31  ->  184     0.0.31          0   /4 ->   =
+18  46      0.3.1   ->  160     3.0.1           3   /4 ->   *
+19  40      0.0.7   ->  40      0.0.7           0   /4 ->   =
+20  10      0.1.1   ->  16      1.0.1           1   /4 ->   *
+21  4       0.0.1   ->  4       0.0.1           0   /4 ->   =
+# "*" if 4a[i+1] occurs in column 3 south, i.e.
+# (4a[i+1]-1) / 3 * 2 = a[i]
