5c44007615ae9413cdbebb7c6f38dfd00dac30eb
Georg Fischer <dr.Georg.Fischer@gmail.com>
2019-04-17 22:49:26 +0200

    running up to 81

--- oeis/luschny/BasicSwingFactorial.pl
+++ oeis/luschny/BasicSwingFactorial.pl
@@ -13,105 +13,126 @@ use Math::BigInt;
 use Math::BigInt':constant'; # $a[$n] = Math::BigInt->new(1);
 # get options
 
-my @smallOddFactorial = map { Math::BigInt->from_hex($_) }
-                                  qw(  0x0000000000000000000000000000001
-    0x0000000000000000000000000000001  0x0000000000000000000000000000001
-    0x0000000000000000000000000000003  0x0000000000000000000000000000003
-    0x000000000000000000000000000000f  0x000000000000000000000000000002d
-    0x000000000000000000000000000013b  0x000000000000000000000000000013b
-    0x0000000000000000000000000000b13  0x000000000000000000000000000375f
-    0x0000000000000000000000000026115  0x000000000000000000000000007233f
-    0x00000000000000000000000005cca33  0x0000000000000000000000002898765
-    0x00000000000000000000000260eeeeb  0x00000000000000000000000260eeeeb
-    0x0000000000000000000000286fddd9b  0x00000000000000000000016beecca73
-    0x000000000000000000001b02b930689  0x00000000000000000000870d9df20ad
-    0x0000000000000000000b141df4dae31  0x00000000000000000079dd498567c1b
-    0x00000000000000000af2e19afc5266d  0x000000000000000020d8a4d0f4f7347
-    0x000000000000000335281867ec241ef  0x0000000000000029b3093d46fdd5923
-    0x0000000000000465e1f9767cc5866b1  0x0000000000001ec92dd23d6966aced7
-    0x0000000000037cca30d0f4f0a196e5b  0x0000000000344fd8dc3e5a1977d7755
-    0x000000000655ab42ab8ce915831734b  0x000000000655ab42ab8ce915831734b
-    0x00000000d10b13981d2a0bc5e5fdcab  0x0000000de1bc4d19efcac82445da75b
-    0x000001e5dcbe8a8bc8b95cf58cde171  0x00001114c2b2deea0e8444a1f3cecf9
-    0x0002780023da37d4191deb683ce3ffd  0x002ee802a93224bddd3878bc84ebfc7
-    0x07255867c6a398ecb39a64b83ff3751  0x23baba06e131fc9f8203f7993fc1495
+my $debug = 1;
+my @smallOddFactorial =             ( "0", "0x0000000000000000000000000000001"
+    , "0x0000000000000000000000000000001", "0x0000000000000000000000000000001"
+    , "0x0000000000000000000000000000003", "0x0000000000000000000000000000003"
+    , "0x000000000000000000000000000000f", "0x000000000000000000000000000002d"
+    , "0x000000000000000000000000000013b", "0x000000000000000000000000000013b"
+    , "0x0000000000000000000000000000b13", "0x000000000000000000000000000375f"
+    , "0x0000000000000000000000000026115", "0x000000000000000000000000007233f"
+    , "0x00000000000000000000000005cca33", "0x0000000000000000000000002898765"
+    , "0x00000000000000000000000260eeeeb", "0x00000000000000000000000260eeeeb"
+    , "0x0000000000000000000000286fddd9b", "0x00000000000000000000016beecca73"
+    , "0x000000000000000000001b02b930689", "0x00000000000000000000870d9df20ad"
+    , "0x0000000000000000000b141df4dae31", "0x00000000000000000079dd498567c1b"
+    , "0x00000000000000000af2e19afc5266d", "0x000000000000000020d8a4d0f4f7347"
+    , "0x000000000000000335281867ec241ef", "0x0000000000000029b3093d46fdd5923"
+    , "0x0000000000000465e1f9767cc5866b1", "0x0000000000001ec92dd23d6966aced7"
+    , "0x0000000000037cca30d0f4f0a196e5b", "0x0000000000344fd8dc3e5a1977d7755"
+    , "0x000000000655ab42ab8ce915831734b", "0x000000000655ab42ab8ce915831734b"
+    , "0x00000000d10b13981d2a0bc5e5fdcab", "0x0000000de1bc4d19efcac82445da75b"
+    , "0x000001e5dcbe8a8bc8b95cf58cde171", "0x00001114c2b2deea0e8444a1f3cecf9"
+    , "0x0002780023da37d4191deb683ce3ffd", "0x002ee802a93224bddd3878bc84ebfc7"
+    , "0x07255867c6a398ecb39a64b83ff3751", "0x23baba06e131fc9f8203f7993fc1495"
     );
 
     sub oddProduct { my ($m, $len) = @_;
         if ($len < 24) {
-            my $p = new BigInt($m);
-            for my $k(2..222) { #  2:2:2(len-1) { ???
-                $p *= ($m - $k);
+            my $p = Math::BigInt->new($m);
+            my $k = 2;
+            while ($k <= 2 * ($len - 1)) { # for k in 2:2:2(len-1) { ???
+                $p->bmul(Math::BigInt->new($m - $k));
+                $k += 2;
             } # for $k
             return $p;
+        } else {
+            my $hlen = $len >> 1;
+            return &oddProduct($m - 2 * $hlen, $len - $hlen)->bmul(&oddProduct($m, $hlen));
         }
-        my $hlen = $len >> 1;
-        return &oddProduct($m - 2 * $hlen, $len - $hlen) * &oddProduct($m, $hlen);
     } # oddProduct
 
     sub oddFactorial { my ($n) = @_;
         my ($sqrOddFact, $oddFact, $oldOddFact);
-        if ($n < scalar(@smallOddFactorial)) { # 41
-            $oddFact    = $smallOddFactorial[1 + $n];
-            $sqrOddFact = $smallOddFactorial[1 + $n / 2];
+        print "oddFactorial(n) with n=$n" if $debug >= 1;
+        if ($n < scalar(@smallOddFactorial) - 1) { # 41
+        	  print ", then part\n" if $debug >= 1;
+            $oddFact    = Math::BigInt->from_hex($smallOddFactorial[1 + $n]);
+            $sqrOddFact = Math::BigInt->from_hex($smallOddFactorial[1 + $n / 2]);
+        #   $oddFact    =                  ($smallOddFactorial[$n]);
+        #   $sqrOddFact =                  ($smallOddFactorial[$n / 2]);
         } else {
+        	  print ", else part\n" if $debug >= 1;
             ($sqrOddFact, $oldOddFact) = &oddFactorial($n / 2);
             my $len = ($n - 1) / 4;
             if ($n % 4 != 2) {
-            	  $len ++;
+                $len ++;
             }
             my $high = $n - (($n + 1) & 1);
-            $oddFact  = $sqrOddFact**2 * (&oddProduct($high, $len) / $oldOddFact);
+        #   high = n - ((n + 1) & 1)
+        #   oddSwing = div(oddProduct(high, len), oldOddFact)
+        #   oddFact = sqrOddFact^2 * oddSwing
+            my $oddSwing = &oddProduct($high, $len)->btdiv($oldOddFact);
+            $oddFact  = $sqrOddFact->bpow(2)->bmul($oddSwing);
         }
         return ($oddFact, $sqrOddFact);
     } # oddFactorial
             
     sub SwingFactorial { my ($n) = @_;
-        my $result = 1;
-        my ($parm) = @_;
+        my $result;
         if ($n < 0) {
             print STDERR "n must be ≥ 0\n";
         } elsif ($n == 0) {
-            $result = 1;
+            $result = Math::BigInt->new(1);
         } else {
-            $result = (&oddFactorial($n))[1] << ($n - &count_ones($n));
+            $result = (&oddFactorial($n))[0]->blsft($n - &popcount($n));
         }
         return $result;
     } # SwingFactorial
+    
+# from <https://www.perlmonks.org/?node_id=1199987>
+sub popcount {
+    my ($parm) = @_;
+    my $result = (Math::BigInt->new($parm)->as_bin()) =~ tr/1//;
+    return $result;
+} # popcount
 
 #START-TEST-################################################
 #   $old2->bmul($old);
 
 # main()
-		my $debug  = 0; # 0 (none), 1 (some), 2 (more)
-		my $number = 29;
-		while (scalar(@ARGV) > 0 and ($ARGV[0] =~ m{\A\-})) { # get options
-		    my $opt = shift(@ARGV);
-		    if (0) {
-		    } elsif ($opt =~ m{d}) {
-		        $debug  = shift(@ARGV);
-		    } elsif ($opt =~ m{n}) {
-		        $number = shift(@ARGV);
-		    } else {
-		        die "invalid option \"$opt\"\n";
-		    }
-		} # while ARGV
+    my $debug  = 0; # 0 (none), 1 (some), 2 (more)
+    my $number = 29;
+    while (scalar(@ARGV) > 0 and ($ARGV[0] =~ m{\A\-})) { # get options
+        my $opt = shift(@ARGV);
+        if (0) {
+        } elsif ($opt =~ m{d}) {
+            $debug  = shift(@ARGV);
+        } elsif ($opt =~ m{n}) {
+            $number = shift(@ARGV);
+        } else {
+            die "invalid option \"$opt\"\n";
+        }
+    } # while ARGV
 
-		my $n;
-		for $n (0..999) {
+    my $n;
+    for $n (0..99) {
+        # print "n = $n\n";
         my $s = &SwingFactorial($n);
-        my $b = (new BigInt($n))->bfac(); # conventionaly with the built-in function
-        if ($s ne $b) {
-        		print STDERR "difference at n = $n\n";
+        my $b = Math::BigInt->new($n);
+        $b->bfac(); # conventional factorial with the built-in function
+        if ($s->bne($b)) {
+            print "difference at n = $n: $s <> $b\n";
         }
     } # for $n
-
+exit;
     # GC.gc() ??
     $n = $number;
-		print "start " . &get_timestamp() . "\n";
-    &SwingFactorial($n);
-		print "end   " . &get_timestamp() . "\n";
-		exit();
+    print "start " . &get_timestamp() . "\n";
+    my $b = Math::BigInt->new($n)->bfac(); 
+    # &SwingFactorial($n);
+    print "end   " . &get_timestamp() . "\n";
+    exit();
 #----
 sub get_timestamp {
     my ($sec, $min, $hour, $mday, $mon, $year, $wday, $yday, $isdst) = localtime (time);
