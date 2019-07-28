55fb90ad7577e9d3a3e5cb8a6f3250d001ebf90e
Georg Fischer <dr.Georg.Fischer@gmail.com>
2019-07-28 15:37:05 +0200

    tested

--- oeis/concatprod/ChakravalaTest.java
+++ oeis/concatprod/ChakravalaTest.java
@@ -14,39 +14,35 @@ import java.lang.Math;
 public class ChakravalaTest {
     public static void main(String[] args) {
         int n = 61;
-        long sqrtn = Math.pow(n, 0.5);
         if (args.length > 0) {
             try {
                 n = Integer.parseInt(args[0]);
             } catch (Exception exc) {
             }
         }
+        long sqrtn = (int) Math.pow(n, 0.5);
         System.out.println("Solve x^2 - " + n + " * y^2 = 1");
-        int size = 32767;
-        long[] p = new long[size];
-        long[] q = new long[size];
-        long[] m = new long[size];
-        long[] x = new long[size];
-        p[0] = (int) Math.pow(n,0.5);
-        q[0] = 1;
-        m[0] = p[0] * p[0] - n;
-        x[0] = p[0];
-        int i = 0;
-        while (m[i] != 1) {
-            System.out.println("p = " + p[i] + ", q = " + q[i] + ", m = " + m[i]);
+        long pn = 0;
+        long po = sqrtn;
+        long qo = 1;
+        long mo = po * po - n;
+        long xo = po;
+        int i = 1;
+        while (mo != 1) {
             int j = 1;
-            long ami = Math.abs(m[i]);
-            while(     j >= sqrtn
+            long ami = Math.abs(mo);
+            while(     j >  sqrtn 
                     || j <= sqrtn - ami
-                    || (j + x[i]) % ami != 0) {
-                j ++;
-            } // while
-            x[i + 1] = j;
-            p[i + 1] = (p[i] * j + n * q[i]) / ami;
-            q[i + 1] = (p[i] + j     * q[i]) / ami;
-            m[i + 1] = (j *    j - n)        / m[i];
-            i ++;
+                    || (j + xo) % (ami) != 0) {
+                j++;
+            } // while j
+            xo = j;
+            pn = (po * j + n * qo) / ami;
+            qo = (po +     j * qo) / ami;
+            mo = (j * j - n) / mo;
+            po = pn;
+            System.out.println("p = " + po + ", q = " + qo + ", m = " + mo + ", x = " + xo);
         } // while i
-        System.out.println("Solution: " + p[i] + "^2 - " + n + " * " + q[i] + "^2 = 1");
-    } //main
-} // ChakravalaTest
+        System.out.println("Solution: " + po + "^2 - " + n + " * " + qo + "^2 = 1");
+    }
+}
