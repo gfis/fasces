893b7deb772d376cdf9b178500c519992a64a7c6
Georg Fischer <dr.Georg.Fischer@gmail.com>
2019-10-01 22:30:18 +0200

    naming

--- oeis/powertrain/powertrain.pl
+++ oeis/powertrain/powertrain.pl
@@ -2,8 +2,10 @@
 
 # powertrain.pl -
 # @(#) $Id$
-# c.f. <https://www.spektrum.de/magazin/powertrain/1669402>
+# 2019-10-01: with hashes
 # 2019-09-24, Georg Fischer
+# c.f. <https://www.spektrum.de/magazin/powertrain/1669402>
+# All variables ending with "b" are in $base representation.
 #:# usage:
 #:#   perl powertrain.pl [-b base] [-m nmax] [-s single] [-d debug]
 #:#       -b base    (default 10)
@@ -23,7 +25,7 @@ my $base    = 10;
 my $debug   = 0;
 my $nsingle = 0;
 my $nmin    = 0;
-my $nmax    = 3000;
+my $nmax    = 30000;
 if (scalar(@ARGV) == 0) {
     print `grep -E "^#:#" $0 | cut -b3-`;
     exit;
@@ -44,45 +46,36 @@ while (scalar(@ARGV) > 0) {
     }
 } # while opt
 
-if ($nmin eq $nmax) { # with -s
-        my $fix = &train($nmin);
-        my $fixb = $fix->to_base($base); # &to_base($fix);
-        if ($debug >= 1 or length($fixb) > 1) {
-            print "$nmin(10) ->\tfixpoint $fix(10)=$fixb($base)\n";
-        }
-} else { # range 1..$nmax
-    my $n = $nmin;
-    while ($n <= $nmax) {
-        my $fix = &train($n);
-        my $fixb = $fix->to_base($base); # &to_base($fix);
-        if ($debug >= 1 or length($fixb) > 1) {
-            print "$n(10) ->\tfixpoint $fix(10)=$fixb($base)\n";
-        }
-        $n ++;
-    } # while $n
-}
+my %pmap = (); # powertrain map
+my %pfix = (); # numbers participating in a fixpoint (cycle)
+# range 1..$nmax
+my $n = $nmin;
+while ($n <= $nmax) {
+    my $fix  = &train($n);
+    my $fixb = &to_base($fix); # $fix->to_base($base); # 
+    if ($debug >= 1 or length($fixb) > 1) {
+        print "$n(10) ->\tfixpoint $fix(10)=$fixb($base)\n";
+    }
+    $n ++;
+} # while $n
 #--------
 sub train {
     my ($n) = @_;
     my $nb = Math::BigInt->new($n);
-    $nb = $nb->to_base($base); # &to_base($n);
-    if ($debug >= 2) {
+    $nb = &to_base($nb); # $nb->to_base($base); # 
+    if ($debug >= 2 or $n % 1000 == 0) {
         print "powertrain($n(10)=$nb($base))\n";
     }
     my $oldnb = 0;
     my $prod = 1;
     while (length($nb) != 1 and $nb ne $oldnb) {
         $oldnb = $nb;
-        if (length($nb) % 2 == 1) { # odd
-            $nb .= "1";
+        if (length($nb) % 2 == 1) { # odd - exponent is missing
+            $nb .= "1"; # assume exponent 1
         }
         my $inb = 0;
-        $prod = Math::BigInt->new(1);
-        while ($inb < length($nb)) {
-            $prod = $prod->bmul(&pow(&to_dec(substr($nb, $inb, 1)), &to_dec(substr($nb, $inb + 1, 1))));
-            $inb += 2;
-        } # while $inb
-        my $prodb = $prod->to_base($base); # &to_base($prod);
+        $prod = &compute($nb);
+        my $prodb = &to_base($prod); # $prod->to_base($base); # 
         if ($debug >= 2) {
             print "$nb($base) -> $prod(10)=$prodb($base)\n";
         }
@@ -91,26 +84,24 @@ sub train {
     return $prod;
 } # train
 
+# compute the product
+sub compute { my ($nb) = @_;
+    my $prod = Math::BigInt->new(1);
+    my $inb = 0;
+    while ($inb < length($nb) and ! $prod->is_zero()) {
+        my $a = &to_dec(substr($nb, $inb    , 1));
+        my $b = &to_dec(substr($nb, $inb + 1, 1));
+        $prod = $prod->bmul(&pow($a, $b));
+        $inb += 2;
+    } # while $inb
+    return $prod;
+} # compute 
+
 # compute a**b - up to 15**14
 sub pow { my ($a, $b) = @_;
-    my $result = 1;
-    if ($b == 0) {
-        # already ok
-    } elsif ($b == 1) {
-        $result = $a;
-    } else {
-        $result = $a ** $b;
-    }
-    if ($debug >= 3) {
-        print "$a ** $b = $result\n";
-    }
-    return $result;
-} # pow
-
-sub pow2 { my ($a, $b) = @_;
     my $result = Math::BigInt->new(1);
     if ($b == 0) {
-        # already ok
+        # 0^0 = 1, already ok
     } elsif ($b == 1) {
         $result = Math::BigInt->new($a);
     } else {
@@ -120,7 +111,7 @@ sub pow2 { my ($a, $b) = @_;
         print "$a ** $b = $result\n";
     }
     return $result;
-} # pow2
+} # pow
 
 # convert from decimal to base, without leading zeroes
 sub to_base { my ($pnum) = @_;
@@ -128,15 +119,17 @@ sub to_base { my ($pnum) = @_;
     my $result = "";
     while ($num->is_positive()) {
         my ($quo, $digit) = $num->bdiv($base);
-        $result =  substr($digits, $digit, 1) . $result;
+        $result = substr($digits, $digit, 1) . $result;
         # $num = $num->bdiv($base);
     } # while > 0
     return $result eq "" ? "0" : $result;
 } # to_base
 
+# convert 1 $base digit back to decimal
 sub to_dec { my ($bdig) = @_;
     return index($digits, $bdig);
 } # to_dec
+#------------------------------------------
 __DATA__
 2592 keinen anderen Fixpunkt.
 Sein Kollege Neil Sloane stellte allerdings fest, dass
@@ -145,54 +138,57 @@ Sein Kollege Neil Sloane stellte allerdings fest, dass
 odd number of digits => append 1
 0^0 = 1
 
+2 - none <= 30000
+3 - none <= 40000
+4 - none <= 30000
+5 - none <= 30000
 
 2960(10) ->     fixpoint 16(10)=24(6)
 
-C:\Users\User\work\gits\fasces\oeis\powertrain>perl powertrain.pl -b 9
+7 - none <= 30000
 
-C:\Users\User\work\gits\fasces\oeis\powertrain>perl powertrain.pl -b 9 -m 20000
 5344(10) ->     fixpoint 24586240(10)=51232874(9)
 6464(10) ->     fixpoint 24586240(10)=51232874(9)
 
-C:\Users\User\work\gits\fasces\oeis\powertrain>perl powertrain.pl -b 7 -m 20000
+642(10) ->      fixpoint 2592(10)=2592(10)
+2164(10) ->     fixpoint 2592(10)=2592(10)
+2534(10) ->     fixpoint 2592(10)=2592(10)
+2592(10) ->     fixpoint 2592(10)=2592(10)
 
-C:\Users\User\work\gits\fasces\oeis\powertrain>perl powertrain.pl -b 9 -m 20000
+11 - none <= 3000
 
-C:\Users\User\work\gits\fasces\oeis\powertrain>perl powertrain.pl -b 9 -m 20000
-5344(10) ->     fixpoint 24586240(10)=51232874(9)
-6464(10) ->     fixpoint 24586240(10)=51232874(9)
-
-C:\Users\User\work\gits\fasces\oeis\powertrain>perl powertrain.pl -b 11 -m 20000
-258(10) ->      fixpoint 10(10)=A(11)
-556(10) ->      fixpoint 10(10)=A(11)
-618(10) ->      fixpoint 10(10)=A(11)
-1018(10) ->     fixpoint 10(10)=A(11)
-2839(10) ->     fixpoint 10(10)=A(11)
-6117(10) ->     fixpoint 10(10)=A(11)
-
-C:\Users\User\work\gits\fasces\oeis\powertrain>perl powertrain.pl -b 12 -m 20000
-305(10) ->      fixpoint 10(10)=A(12)
-486(10) ->      fixpoint 486(10)=346(12)
-494(10) ->      fixpoint 486(10)=346(12)
-509(10) ->      fixpoint 39366(10)=1A946(12)
-534(10) ->      fixpoint 39366(10)=1A946(12)
-542(10) ->      fixpoint 39366(10)=1A946(12)
-734(10) ->      fixpoint 10(10)=A(12)
-1326(10) ->     fixpoint 486(10)=346(12)
-1337(10) ->     fixpoint 39366(10)=1A946(12)
-1350(10) ->     fixpoint 39366(10)=1A946(12)
-1517(10) ->     fixpoint 39366(10)=1A946(12)
-3641(10) ->     fixpoint 486(10)=346(12)
-3645(10) ->     fixpoint 39366(10)=1A946(12)
 3661(10) ->     fixpoint 10(10)=A(12)
 5833(10) ->     fixpoint 486(10)=346(12)
-5929(10) ->     fixpoint 486(10)=346(12)
-6109(10) ->     fixpoint 39366(10)=1A946(12)
-6279(10) ->     fixpoint 39366(10)=1A946(12)
-6409(10) ->     fixpoint 39366(10)=1A946(12)
 6505(10) ->     fixpoint 39366(10)=1A946(12)
 
-C:\Users\User\work\gits\fasces\oeis\powertrain>perl powertrain.pl -b 19 -m 20000
+13 - none <= 30000
+14 - none <= 30000
+15 - none <= 30000
+
+25143(10) ->    fixpoint 78732(10)=1338c(16)
+
+29652(10) ->    fixpoint 10000(10)=20a4(17)
+
+24679(10) ->    fixpoint 768(10)=26c(18)
+
 1604(10) ->     fixpoint 524288(10)=40862(19)
 1617(10) ->     fixpoint 524288(10)=40862(19)
 3004(10) ->     fixpoint 524288(10)=40862(19)
+
+124(10) ->      fixpoint 1296(10)=34g(20)
+
+937(10) ->      fixpoint 1024(10)=26g(21)
+
+27082(10) ->    fixpoint 2048(10)=452(22)
+
+23 -none <= 30000
+
+4116(10) ->     fixpoint 4116(10)=73c(24)
+8133(10) ->     fixpoint 4116(10)=73c(24)
+
+25 -none <= 30000
+
+85(10) ->       fixpoint 2187(10)=363(26)
+
+27 -none <= 30000
+28 -none <= 10000
