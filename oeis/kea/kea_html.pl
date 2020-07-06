ea9ba6ef8f742c7f32409ddc31f1c8899fe3c039
Georg Fischer <dr.Georg.Fischer@gmail.com>
2020-07-06 12:19:48 +0200

    strands ok

--- oeis/kea/kea_html.pl
+++ oeis/kea/kea_html.pl
@@ -1,32 +1,32 @@
 #!perl
 
-# Build the Kimberling expulsion array in a triangle in HTML
-# and show lines of known values
+# Build the (active part of) Kimberling's expulsion (or shuffle) array in a triangle in HTML
 # @(#) $Id$
+# 2020-07-05: with strands; MH=72
 # 2019-01-09: var. renamed
 # 2018-08-07: 1st and 2nd level known values (delta 3)
 # 2018-05-07, Georg Fischer
 #--------------------------------------------------------
 # usage:
-#   perl kea_html.pl [maxrow] [-c] [-k]
-#       -b base  (default 10), maybe 9
-#       -c center
-#       -k known values
-#       -p known places
+#   perl kea_html.pl [maxrow] [-base 10] [-cent {0|1}] [-html {0|1}] [-known {0|1}] [-s {0|1}]
+#       maxrow  number of row to compute (default 64)
+#       -base   (default 10), maybe 9
+#       -cent   whether to center the active triangle
+#       -html   whether to output HTML  
+#       -known  whether to colorize known NE-SW lines (cold)
+#       -s      whether to compute strands and colorize them (warm) 
 #--------------------------------------------------------
 use strict;
 use integer;
-# use warnings;
+use warnings;
 
-my $maxrow  = 100;
-my $center  = 0;
-my $html    = 1;
-my $known   = 0;
-my $maind   = 0;
-my $places  = 0;
-my $subdiag = 0;
-my $debug   = 0;
+my $maxrow  = 64;
 my $base    = 10; # for &to_base
+my $center  = 0; 
+my $debug   = 0;
+my $html    = 1;
+my $known   = 0; # cold colors
+my $strand  = 1; # warm colors
 my @digits  = qw(0 1 2 3 4 5 6 7 8 9 a b c d e f g h i j k l m n o p q r s t u v w x y z); # for &to_base
 while (scalar(@ARGV) > 0) {
     my $opt = shift(@ARGV);
@@ -43,18 +43,14 @@ while (scalar(@ARGV) > 0) {
         $html    = shift(@ARGV);
     } elsif ($opt =~ m{\A-known}) {
         $known   = shift(@ARGV);
-    } elsif ($opt =~ m{\A-main}) {
-        $maind   = shift(@ARGV);
-    } elsif ($opt =~ m{\A-place}) {
-        $places  = shift(@ARGV);
-    } elsif ($opt =~ m{\A-sub}) {
-        $subdiag = shift(@ARGV);
+    } elsif ($opt =~ m{\A-s}) {
+        $strand = shift(@ARGV);
     } else {
         die "invalid option \"$opt\"\n";
     }
 } # while $opt
 #--------
-my @kear; # left lower triangle of Kimberling's expulsion array; valid elements in a row extend from 1 to 2*rowno - 1
+my @kear; # left lower triangle of Clark Kimberling's expulsion array; valid elements in a row extend from 1 to 2*rowno - 1
 my @clar; # assembled class attribute for $kear[i,j]
 my @kfol; # whether the cell was visited when +3 chains are followed
 $kear[0][0] = 0; # not used
@@ -80,7 +76,7 @@ if (0) { # follow chains - not used
     } # while advancing
 } # if follow
 
-my @queue  = (0);  # diagonal index still to be splitted, 0 = main diagonal
+my @queue  = (0);  # strand index still to be splitted, 0 = main diagonal
 my @srow   = (1);  # starting element row    number
 my @scol   = (1);  # starting element column number
 my @drow   = (1);  # delta  for rows    (2^k for k >= 0; k=0 for main diag.)
@@ -88,16 +84,21 @@ my @dcol   = (1);  # delta  for columns (2*n - 1 for 1 <= n < 2^k)
 my @level  = (0);  # 0 = main diagonal
 my @parent = (-1); # main diagonal has no parent
 my @parity = (0);  # treat main diagonal as "even"
+my $old_level = 0;
 
-if ($subdiag == 1) { # compute subdiagonals
-    my $qhead  = 0; # next queue element to be splitted
+if ($strand == 1) { # compute substrands
+    my $qhead = 0; # next queue element to be splitted
     while ($qhead < scalar(@queue)) { # queue not empty
-        my $iqueue = $queue[$qhead]; # split this one
-        &split_diagonal($iqueue, 0);
-        &split_diagonal($iqueue, 1);
+        my $istrand = $queue[$qhead]; # split this one
+        if ($old_level != $level[$istrand]) {
+            $old_level =  $level[$istrand];
+            print "<!-- level $old_level -->\n";
+        }
+        &split_strand($istrand, 0);
+        &split_strand($istrand, 1);
         $qhead ++;
     } # while not empty
-} # if subdiagonals
+} # if substrands
 
 if ($html == 1) {
     &set_attributes();
@@ -112,70 +113,85 @@ if ($html == 1) {
 } # if html
 # end main
 #================================================================
-sub split_diagonal { # queue the next even or odd subdiagonal
-    my ($iqueue, $offset) = @_; # offset = 0 or 1
-    my ($irow0, $icol0) = ($srow[$iqueue], $scol[$iqueue]);
+sub split_strand { # queue the next even or odd substrand
+    my ($istrand, $offset) = @_; # offset = 0 or 1
+    my ($irow0, $icol0) = ($srow[$istrand], $scol[$istrand]);
     if ($debug >= 2) {
-        print STDERR "# start split_diagonal(iqueue=$iqueue, par=$offset) \@$irow0,$icol0\n";
+        print STDERR "# start split_strand(istrand=$istrand, par=$offset) \@$irow0,$icol0\n";
     } 
     if ($offset == 0) { # even - start with first element
     } else { # odd - start with second element
-        $irow0 += $drow[$iqueue];
-        $icol0 += $dcol[$iqueue];
+        $irow0 += $drow[$istrand];
+        $icol0 += $dcol[$istrand];
     } # odd
     my  $term0 =  &get($irow0, $icol0);
-    my  $krow0 =  $irow0 - 1; # -> mapped element of target diagonal
-    my  $kcol0 =  &search_term($krow0, $term0);
+    my  $krow0 =  $irow0 - 1; # -> mapped element of target strand
+    my  $kcol0 =  &search_term($krow0, $term0, 0);
     while($kcol0 < 0 and $krow0 < $maxrow) { # not found 
-        $irow0 += 2 * $drow[$iqueue]; # advance in source diagonal
-        $icol0 += 2 * $dcol[$iqueue];
+        $irow0 += 2 * $drow[$istrand]; # advance in source strand
+        $icol0 += 2 * $dcol[$istrand];
         $term0 =  &get($irow0, $icol0); 
         $krow0 =  $irow0 - 1;
-        $kcol0 =  &search_term($krow0, $term0);
+        $kcol0 =  &search_term($krow0, $term0, 1);
     } # while term0 not found
-    my  $irow1 =  $irow0 + 2 * $drow[$iqueue];
-    my  $icol1 =  $icol0 + 2 * $dcol[$iqueue];
-    my  $term1 =  &get($irow1, $icol1);
-    my  $krow1 =  $irow1 - 1; # -> mapped element of target diagonal
-    my  $kcol1 =  &search_term($krow1, $term1);
-    if ($kcol0 >= 0 and $kcol1 >= 0) { # two valid subdiagonal elements found
-        my $inext = scalar(@srow);
-        $srow  [$inext] = $krow0;
-        $scol  [$inext] = $kcol0;
-        $drow  [$inext] = $krow1 - $krow0;
-        $dcol  [$inext] = $kcol1 - $kcol0;
-        $parent[$inext] = $iqueue;
-        $level [$inext] = $level[$iqueue] + 1;
-        $parity[$inext] = $offset;
-        push(@queue, $inext);
-        if ($debug >= 0) {
-            print join("\t", "# diag $inext:", "\@$srow[$inext],$scol[$inext]", "\+$drow[$inext],$dcol[$inext]"
-                , "**$level[$inext]", , "\^$parent[$inext]", $parity[$inext] == 0 ? "even" : "odd") . "\n";
-        } 
-    } # valid subdiagonal found
-} # split_diagonal
+    if ($term0 >= 0 and $kcol0 >= 0) { # term0 valid
+        my  $irow1 =  $irow0 + 2 * $drow[$istrand];
+        my  $icol1 =  $icol0 + 2 * $dcol[$istrand];
+        my  $term1 =  &get($irow1, $icol1);
+        my  $krow1 =  $irow1 - 1; # -> mapped element of target strand
+        my  $kcol1 =  &search_term($krow1, $term1, 2);
+        if ($term1 >= 0 and $kcol1 >= 0) { # term1 valid - two substrand elements found
+            my $inext = scalar(@srow);
+            push(@queue, $inext);
+            $parent[$inext] = $istrand;
+            $level [$inext] = $level[$istrand] + 1;
+            $parity[$inext] = $offset;
+            $srow  [$inext] = $krow0;
+            $scol  [$inext] = $kcol0;
+            $drow  [$inext] = $krow1 - $krow0;
+            $dcol  [$inext] = $kcol1 - $kcol0;
+            my $mrow = $krow0; # minimal distance to origin, or mod
+            my $mcol = $kcol0;
+            while (
+                    $mrow >= $drow[$inext]
+                  or 
+                  0 # $mcol >= $dcol[$inext]
+                  ) { 
+                $mrow -= $drow[$inext];
+                $mcol -= $dcol[$inext];
+            } # whil not mod
+            if ($debug >= 0) {
+                print "<!--" . join("\t", sprintf("%5d:", $inext), "\@$srow[$inext],$scol[$inext]", "\+$drow[$inext],$dcol[$inext]"
+                    , "2^$level[$inext]", "$parent[$inext]," . ($parity[$inext] == 0 ? "ev" : "od"), "m$mrow,$mcol") . " -->\n";
+            } 
+        } # valid term1 / substrand found
+    } # valid term0 found
+} # split_strand
 #----------------
 sub search_term { # return the column number where $term occurs in row $irow
-    my ($irow, $term)  = @_;
+    my ($irow, $term, $id)  = @_;
     if ($debug >= 2) {
         print STDERR "# start search_term($irow, $term)\n";
     } 
     my $icol = 1;
     my $ff_col = 2 * $irow;
-    my $busy = 1; # not (yet) found
-    while ($busy >= 1 and $icol < $ff_col) {
+    my $busy = $irow <= $maxrow ? 1 : 2; # not (yet) found
+    while ($busy == 1 and $icol < $ff_col) {
+        if (! defined($kear[$irow][$icol])) {
+            print "# undef kear[$irow][$icol] while searching for $term with id=$id\n";
+        }
         if ($kear[$irow][$icol] == $term) {
             $busy = 0; # found
         } else {
             $icol ++;
         }
     } # while busy
-    return $busy == 1 ? -1 : $icol; # -1 if not found
+    return $busy >= 1 ? -1 : $icol; # -1 if not found
 } # search_term
 #----------------
 sub get { # return element [$irow, $icol] if it is in the active array, of -1 if not in range
     my ($irow, $icol)  = @_;
-    return $icol >= 1 && $icol < 2 * $irow ? $kear[$irow][$icol] : -1;
+    return ($irow <= $maxrow and $icol >= 1 and $icol < 2 * $irow) ? $kear[$irow][$icol] : -1;
 } # get
 #----------------
 sub fill_next_row { # compute next row [$irow + 1]
@@ -245,11 +261,10 @@ sub set_attributes { # colorize elements which have special attributes
             $start += $delta;
         } # while $start <= $maxrow
     } # known values
-    if ($maind  > 0) {
+    if ($strand > 0) { # |-> means: position in next row; last interesting element is $kear[i][2*i-1]
         # main diagonal,   darkred
         &line(  1,  1,  1,  1, "d0"); # 0,0
-    }
-    if ($places > 0) { # |-> means: position in next row; last interesting element is $kear[i][2*i-1]
+
         # 1st derivatives, crimson
         &line(  3,  1,  2,  1, "d1"); # 1,0               d1,1  |->od0,1  left  4,7,8,9,24,14 ...
         &line(  4,  7,  2,  3, "d1"); # 0,1               d1,3  |->ed0,1  right 10,15,20,18,31 ...
@@ -265,10 +280,10 @@ sub set_attributes { # colorize elements which have special attributes
         &line( 22,  1,  8,  1, "d3"); # -2,-2             d3,1  |->ed2,7  2,34,58,82
         &line(  5,  1,  8,  3, "d3"); # -3,-2             d3,3  |->od2,5  8,22,23
         &line(  7,  4,  8,  5, "d3"); # -1,1              d3,5  |->od2,3  9,35,55,48
-        &line(  4,  4,  8,  7, "d3"); # 4,4               d3,7  |->ed2,1  4,28,36,54
+        &line( 12, 11,  8,  7, "d3"); # 4,4               d3,7  |->ed2,1  4,28,36,54
         #--
         &line(  8,  9,  8,  9, "d3"); # 0,0               d3,9  |->ed2,1  18,33,62,70
-        &line( 11, 16,  8, 11, "d3"); # 3,5               d3,11 |->od2,3  14,6
+        &line(  3,  5,  8, 11, "d3"); # 3,5               d3,11 |->od2,3  14,6
         &line(  9, 16,  8, 13, "d3"); # 1,3               d3,13 |->od2,5  24,46,59
         &line( 26, 51,  8, 15, "d3"); # 2,6               d3,15 |->ed2,7  76,99
 
@@ -277,7 +292,7 @@ sub set_attributes { # colorize elements which have special attributes
         &line( 24,  3, 16,  3, "d4"); # 8,0               d4,3  |-> d3,13 59,13
         &line( 10,  2, 16,  5, "d4"); # -6,1              d4,5  |-> d3,11 14,65
         &line( 15,  6, 16,  7, "d4"); # -1,5              d4,7  |-> d3,9  33,70
-        &line(  3,  1, 16,  9, "d4"); # 3,1               d4,9  |-> d3,7  4,36,44
+        &line( 19, 10, 16,  9, "d4"); # 3,1               d4,9  |-> d3,7  4,36,44
         &line(  6,  4, 16, 11, "d4"); # 6,4               d4,11 |-> d3,5  9,55
         &line( 12, 10, 16, 13, "d4"); # -4,-3             d4,13 |-> d3,3  22,11,115
         &line( 29, 28, 16, 15, "d4"); # -3,-2             d4,15 |-> d3,1  34.82
@@ -326,10 +341,10 @@ sub follow_chains {
             my $busy = 1;
             my $kcol = 1;
             while ($busy == 1 and $kcol < $irow * 2) {
-                if ($elem + 3 <= $kear[$kcol][$kcol]) { # found in diagonal
+                if ($elem + 3 <= $kear[$kcol][$kcol]) { # found in main diagonal
                     push(@in_diags, $elem);
                     $busy = 0;
-                } # found in diagonal
+                } # found in main diagonal
                 $kcol ++;
             } # while $kcol
             if (0 and $busy == 0) {
@@ -461,7 +476,7 @@ tr,td,th,p
 .k3     { background-color: lightblue; color: black; font-weight: bold; /* font-style: italic; */ }
 .k4     { background-color: lavender;  color: black; font-weight: bold; /* font-style: italic; */ }
 .k5     { background-color: lavender;  color: black; font-weight: bold; /* font-style: italic; */ }
-/* main diagonal and its derivatives */
+/* main diagonal and its strands */
 .d0     { background-color: darkred;   color: white; font-weight: bold; }
 .d1     { background-color: crimson;   color: white; }
 .d2     { background-color: orangered; color: white; }
@@ -479,19 +494,19 @@ tr,td,th,p
 <td class="k0">   &nbsp; &nbsp; &nbsp; </td><td style="text-align:left;">known values</td>
 </tr>
 <tr>
-<td class="d1">   &nbsp; &nbsp; &nbsp; </td><td>1st level derived diagonals</td>
+<td class="d1">   &nbsp; &nbsp; &nbsp; </td><td>1st level derived strands</td>
 <td class="k1">   &nbsp; &nbsp; &nbsp; </td><td style="text-align:left;">1st level known delta 3</td>
 </tr>
 <tr>
-<td class="d2">   &nbsp; &nbsp; &nbsp; </td><td>2nd level derived diagonals</td>
+<td class="d2">   &nbsp; &nbsp; &nbsp; </td><td>2nd level derived strands</td>
 <td class="k2">   &nbsp; &nbsp; &nbsp; </td><td style="text-align:left;">2nd level known delta 3</td>
 </tr>
 <tr>
-<td class="d3">   &nbsp; &nbsp; &nbsp; </td><td>3rd level derived diagonals</td>
+<td class="d3">   &nbsp; &nbsp; &nbsp; </td><td>3rd level derived strands</td>
 <td class="k3">   &nbsp; &nbsp; &nbsp; </td><td style="text-align:left;">3rd level known delta 3</td>
 </tr>
 <tr>
-<td class="d4">   &nbsp; &nbsp; &nbsp; </td><td>4th level derived diagonals</td>
+<td class="d4">   &nbsp; &nbsp; &nbsp; </td><td>4th level derived strands</td>
 <td class="k4">   &nbsp; &nbsp; &nbsp; </td><td style="text-align:left;">4th level known delta 3</td>
 </tr>
 <tr>
