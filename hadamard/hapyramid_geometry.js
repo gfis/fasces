56e0f6137740a9097d861ddc3f2e904e299d7f96
Georg Fischer <dr.georg.fischer@gamil.com>
2025-08-08 15:18:25 +0200

    single blocks

--- hadamard/hapyramid_geometry.js
+++ hadamard/hapyramid_geometry.js
@@ -12,15 +12,9 @@
 				var center = new THREE.Vector3( 0, 0, 0 );
 				var size   = 300;
 				var size5  = parseInt(size / 5);
-				var corner = 4; // number of corners of the cylinders
-				var thick  = parseInt(size5 / 4); // thickness of the cylinders
-				var vec_Fs = [0,1,2,3,4,14,24,34,33,23,13,12,22,32,31,21,11,10,20,30,40,41,42,43,44
-					,144,244,344,343,243,143,142,242,342,341,241,141,140,240,340,330,230,130,120,220
-					,320,310,210,110,111,211,311,321,221,121,131,231,331,332,232,132,122,222,322,312
-					,212,112,113,213,313,323,223,123,133,233,333,334,234,134,124,224,324,314,214,114
-					,104,204,304,303,203,103,102,202,302,301,201,101,100,200,300,400,401,402,403,404
-					,414,424,434,433,423,413,412,422,432,431,421,411,410,420,430,440,441,442,443,444
-					,444];
+				var corner = 4; // number of corners of the blocks
+				var thick  = parseInt(size5 / 1); // thickness of the blocks
+				var vec_Fs = [323,223,123,122,222,322,332,333];
 				var vec = vec_Fs;
 				var materials = [
 					new THREE.MeshBasicMaterial( { color: 0xffffff, overdraw: 0.5 } ),
@@ -36,7 +30,7 @@
 					var y = center.y + (digy5 - 2) * size5;
 					var z = center.z + (digz5 - 2) * size5;
 					points.push(new THREE.Vector3(x, y, z));
-					if (false) { // with annotation: node values
+					if (true) { // with annotation: node values
 						var geometry = new THREE.TextGeometry( vec[i],
 							{ font: myfont
 							, size: 6
@@ -44,9 +38,9 @@
 							, curveSegments: 8
 							} );
 						var mesh = new THREE.Mesh( geometry, materials );
-						mesh.position.x = x + thick;
-						mesh.position.y = y + thick;
-						mesh.position.z = z + thick;
+						mesh.position.x = x;
+						mesh.position.y = y;
+						mesh.position.z = z;
 						var group = new THREE.Group();
 						group.add( mesh );
 						scene.add( group );
@@ -54,7 +48,7 @@
 				} // for i
 				var shift = (size5 - thick) * 0.5;
 				for ( i = 0; i < points.length - 1; i ++ ) {
-				//var geometry = new THREE.BoxGeometry( thick, size5, thick );
+				//var geometry = new THREE.BoxGeometry( thick, size5, thick ); // this made blocks of length 
 					var geometry = new THREE.BoxGeometry( thick, thick, thick );
 					var diff = vec[i + 1] - vec[i]; // either +-1, +-10 or +-100
 					var adiff = diff;
